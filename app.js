7ca18b2210fbbf970d8743a8b1755b8ca8e27ff8
tai-fujio <taisaburo.fujio@gmail.com>
2020-08-26 18:29:52 +0900

    mongodb setup

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const graphqlHTTP = require('express-graphql');
 const app = express();
+const mongoose = require('mongoose');
 
-app.use('/graphql', graphqlHTTP);
+mongoose.connect('mongodb+srv://user:user2@mydb.lzuap.mongodb.net/mydb?retryWrites=true&w=majority');
+mongoose.connection.once('open', () => {
+  console.log('db connected')
+});
+app.use('/graphql', graphqlHTTP({
+
+}));
 app.listen(3001, () => {
   console.log('listening');
 })
