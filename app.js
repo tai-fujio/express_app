2656d8ea1ca9ddeb4a66ca45bf45997c41485208
tai-fujio <taisaburo.fujio@gmail.com>
2020-08-26 15:57:16 +0900

    graphqlハンドラ

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const graphqlHTTP = require('express-graphql');
 const app = express();
 
+app.use('/graphql', graphqlHTTP);
 app.listen(3001, () => {
   console.log('listening');
 })
