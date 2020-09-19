7c9707ce7f21e92c5d1fc6358ce02a7a485157fd
tai-fujio <51994563+tai-fujio@users.noreply.github.com>
2020-09-19 23:23:58 +0900

    ディレクトリ移動

--- graphql/app.js
+++ graphql/app.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const express = require("express");
 const graphqlHTTP = require('express-graphql');
 const mongoose = require('mongoose');
 const schema = require('./schema/schema')
 const app = express();
 
-mongoose.connect('mongodb+srv://user:user2@mydb.lzuap.mongodb.net/sample?retryWrites=true&w=majority');
+mongoose.connect(process.env.MONGO_URL);
 mongoose.connection.once('open', () => {
   console.log('db connected')
 });
