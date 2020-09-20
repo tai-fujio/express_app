25ac2ca1e73f0e4fe7b76056b53e4eaeea96f7ed
tai-fujio <51994563+tai-fujio@users.noreply.github.com>
2020-09-20 11:16:23 +0900

    eslint config

---
+++ app/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "indent": [
+      "error",
+      2,
+      { "SwitchCase": 1 }
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-unused-vars": [
+      "error",
+      {
+        "vars": "all",
+        "args": "none",
+      }
+    ],
+    "no-console": [
+      "off"
+    ]
+  }
+};
