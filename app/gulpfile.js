8a94f949aa5a0e4ad857bebdf4f60a2d71bd6a35
tai-fujio <51994563+tai-fujio@users.noreply.github.com>
2020-10-02 19:50:48 +0900

    gulp(ver.3) settings

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -5,10 +5,17 @@ let development, production;
 load("./gulp/tasks", { recurse: true });
 
 development = [
+  "copy-third_party",
+  "copy-images",
+  "copy-javascripts",
+  "compile-sass",
 ];
 
 production = [
-  "compile-sass"
+  "copy-third_party",
+  "copy-images",
+  "uglify-javascripts",
+  "compile-sass",
 ];
 
 gulp.task("default", config.env.IS_DEVELOPMENT ? development : production);
