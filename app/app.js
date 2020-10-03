4d0c5eca1eb151d0c65e143526355a2a9291020f
tai-fujio <51994563+tai-fujio@users.noreply.github.com>
2020-10-03 21:20:33 +0900

    set up top page boilerplate

--- app/app.js
+++ app/app.js
@@ -1,2 +1,9 @@
 /* eslint-env node */
 console.log(process.env.NODE_ENV);
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+app.disable("x-powered-by");
+app.use("/public", express.static(__dirname + "/public/" + (process.env.NODE_ENV === "development" ? "development" : "production")));
+app.use("/", require("./routes/index.js"));
+app.listen(3000);
